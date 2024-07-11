f692f7b30c27e2f9f3a414392fc003f36c46eed2
Roberto Gutiérrez <robertoargs@gmail.com>
2024-07-11 11:54:29 -0500

    Secret Scanning Feature

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import secrets
+
 MY_TOKEN = "gh_35#26c6818f8c73caa9eaf2a6dac@9aaed7"
 
 print("This is an example to try Secret Scanning Alerts")
 
+MY_SECRET = "34e0a8a4514f53e59168e2b9db7f4d6e"
+
+print("This is another example to try Secret Scanning Alerts")
+print("SECRET ===>", MY_SECRET)
